d86f4cea24019c9d08c77336abd8f92c5c4e5f84
Yuli <yuli@ip-192-168-0-105.ap-southeast-1.compute.internal>
2025-02-03 19:15:29 +0700

    Costumize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Theme } from '@radix-ui/themes';
 import Navbar from "./Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
@@ -28,11 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="violet">
           <Navbar />
           <main className="p-5"> {children}</main>
+          {/* <ThemePanel /> -> to show all colors available */}
         </Theme>
       </body>
     </html>

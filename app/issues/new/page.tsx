e3ce8bf9665c7bb5ce681142b6df73533b3e5f30
Yuli <yuli@Khanas-MacBook-Pro.local>
2025-02-05 20:19:52 +0700

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,22 @@
 "use client"
-import { TextField, Button, Callout } from '@radix-ui/themes'
+import { TextField, Button, Callout, Text } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from "zod"
 
-interface IssueForm {
-    title: string,
-    description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
     const router = useRouter()
-    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
     const [error, setError] = useState('')
 
     return (
@@ -34,12 +36,14 @@ const NewIssuePage = () => {
                     }
                 })}>
                 <TextField.Root placeholder='Title' {...register('title')} />
+                {errors.title && <Text color="red" as="p">{errors.title.message /* this title.message is gotton from validation in validationSchema.ts */ }</Text>} 
                 <Controller 
                     name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder='Description' { ...field } />} // field here is as the substitution of register
 
                 />
+                {errors.description && <Text color="red" as="p">{errors.description.message /* this description.message is gotton from validation in validationSchema.ts */}</Text>}
                 <Button>Submit New Issue</Button>
             </form>
         </div>
